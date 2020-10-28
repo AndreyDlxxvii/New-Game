b7194325d0d0d00d1cca1390469eef0a32ce7e7b
AndreyDlxxvii <Dlxxvii@gmail.com>
2020-10-28 15:06:46 +0300

    Start new game

---
+++ Assets/Scripts/Control.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Control : MonoBehaviour
+{
+    public float Speed = 5f;
+    public float JumpForce = 100f;
+
+    private bool _isGrounded;
+    private Rigidbody _rb;
+
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        Movement();
+        Jump();
+    }
+
+    private void Movement()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        _rb.AddForce(movement * Speed);
+    }
+
+    private void Jump()
+    {
+        if (Input.GetAxis("Jump") > 0)
+        {
+            if (_isGrounded)
+            {
+                _rb.AddForce(Vector3.up * JumpForce);
+            }
+        }
+    }
+
+    void OnCollisionEnter (Collision collision)
+        {
+            IsGroundedUpate(collision, true);
+        }
+
+        void OnCollisionExit(Collision collision)
+        {
+            IsGroundedUpate(collision, false);
+        }
+
+        void IsGroundedUpate(Collision collision, bool value)
+        {
+            if (collision.gameObject.tag == ("Ground"))
+            {
+                _isGrounded = value;
+            }
+        }
+    }
+
